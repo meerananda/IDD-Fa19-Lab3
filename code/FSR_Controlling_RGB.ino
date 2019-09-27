0035f13054bd706d4bb87ea3a02054aac4f336d4
meerananda <meerananda91@gmail.com>
2019-09-26 20:25:09 -0400

    Add files via upload

---
+++ code/FSR_Controlling_RGB.ino
@@ -0,0 +1,51 @@
+// References: https://forum.arduino.cc/index.php?topic=100421.0
+
+int fsrAnalogPin = A0; // FSR is connected to analog 0
+
+int redpin = 11;      // connect ___ LED to pin 11 (PWM pin)
+int greenpin = 10;      // connect ___ LED to pin 10 (PWM pin)
+int bluepin = 9;      // connect ___ LED to pin 9 (PWM pin)
+
+int fsrValue;      // the analog reading from the FSR resistor divider
+
+int red, green, blue;
+int rgbValue;
+
+void setup(void) {
+ Serial.begin(9600);   // We'll send debugging information via the Serial monitor
+ pinMode(redpin, OUTPUT);
+ pinMode(greenpin, OUTPUT);
+ pinMode(bluepin, OUTPUT);
+ pinMode(fsrAnalogPin, INPUT);
+}
+
+void loop()
+{
+  //Get the analog reading from the FSR
+  fsrValue = analogRead(fsrAnalogPin);
+  rgbValue = map(raw, 0, 1000, 0, 765);
+
+  //Assign the readings to each pin depending on the range they fall within.
+  //Also allows for overlapping of colors to have a smooth transition.
+  if (rgbValue <= 255) {
+    red = rgbValue;
+    green = 0;
+    blue = 0;
+  }
+  // Readings from 
+  if (rgbValue > 255 && rgbValue <= 510) {
+    red = 510 - rgbValue;
+    green = rgbValue - 255;
+    blue = 0;
+  }
+  if (rgbValue > 510 && rgbValue <= 765) {
+    red = 0;
+    green = 765 - rgbValue;
+    blue = rgbValue - 510;
+  }
+
+  //Write the values to each pin
+  analogWrite(redpin, red);
+  analogWrite(greenpin, green);
+  analogWrite(bluepin, blue);
+}
